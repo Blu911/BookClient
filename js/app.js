85a4cc78d1f442958023565fb3443f0c2ff4cf21
Krystian Woźniak <krys.blu@gmail.com>
2019-01-25 15:08:02 +0100

    Pobieranie ksiazek i wyswietlanie szczegolow

---
+++ js/app.js
@@ -0,0 +1,50 @@
+$('document').ready(function () {
+
+    getAllBooks();
+
+    $('#books').on('click', 'li', function () {
+      $(this).next().slideToggle();
+    });
+
+});
+
+function getBook(id) {
+    $.ajax({
+        url: 'http://localhost:8282/books'+'/'+id,
+        type: 'GET',
+        dataType: 'json'
+    }).done(function (book) {
+        console.log(book);
+        // showMore(book);
+    });
+}
+
+
+
+
+function getAllBooks() {
+    $.ajax({
+        url: 'http://localhost:8282/books',
+        type: 'GET',
+        dataType: 'json'
+    }).done(function (books) {
+        display(books)
+    });
+}
+
+
+function display (books) {
+
+    var ul =$('#books');
+
+    for (var i = 0; i < books.length; i++) {
+        var newLi = $('<li>').text(books[i].title).attr('data-id', books[i].id);
+        var newDiv = $('<div>').text(books[i].author + " " + books[i].publisher).attr('data-id', books[i].id).hide();
+        ul.append(newLi).append(newDiv);
+
+    }
+
+
+}
+
+
